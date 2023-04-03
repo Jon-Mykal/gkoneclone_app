89ca5aaac97b1d8f2cf86673b762c4f71554a054
Gregg Wong <gwong@smartterm.io>
2023-04-03 17:05:19 -0500

    Add Navigator

--- App.js
+++ App.js
@@ -1,14 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import StartPage from './views/StartPage';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen name="Start" component={StartPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app</Text>
+
+    //   <StatusBar style="auto" />
+    // </View>
+    // <StartPage />
   );
 }
+
+const StartPage2 = () => (
+  <View style={{ backgroundColor: 'red', width: '100%', height: '100%' }}>
+    <Pressable onPress={() => console.log('pressed')}>
+      <Text>Press me</Text>
+    </Pressable>
+   { /* <View style={{
+      paddingTop: 150, alignItems: 'center'
+    }}>
+      <Text style={{ fontSize: 25, color: "white" }}>Welcome to</Text>
+      <Text style={{ fontSize: 40, color: "white", fontWeight: "bold" }}>
+        GK <Text style={{ color: "black" }}>One</Text>
+      </Text>
+    </View>
+    <View style={{ paddingTop: '30vh' }}>
+      {/* <Pressable style={{
+        backgroundColor: "white",
+        ...styles.buttonSolid
+      }}>
+        <Text style={{ color: 'red', fontWeight: "bold" }}>Create an account</Text>
+      </Pressable> */}
+      {/* <View style={{ marginVertical: 8 }}></View> */}
+      {/* <Pressable style={({ pressed }) => [
+        {
+          backgroundColor: pressed ? 'white' : 'red',
+          transition: 'all ease .2s'
+        },
+        styles.buttonOutline
+      ]}>
+        {({ pressed }) => (
+          <Text style={
+            pressed ? { ...styles.text, color: "red" } : styles.text
+          }>Log in</Text>)
+        }
+      </Pressable> 
+    </View>*/}
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
