be06e07dc9ff4a6d8dfe1af0817da3f5ab442859
Gregg Wong <gwong@smartterm.io>
2023-04-03 19:03:25 -0500

    Add CreateAccountPage UI

--- views/CreateAccountPage.js
+++ views/CreateAccountPage.js
@@ -1,3 +1,13 @@
 import React from "react";
 import { View, Text, Button, TouchableOpacity, Pressable, StyleSheet } from "react-native";
 
+const CreateAccountPage = () => {
+    return (
+        <View>
+            <Text>Create Account Page</Text>
+        </View>
+    );
+}
+
+export default CreateAccountPage;
+
