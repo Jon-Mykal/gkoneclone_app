e78897d04d10c58beba61bdf41e148dfbab09f44
Gregg Wong <gwong@smartterm.io>
2023-04-04 17:09:38 -0500

    Add Wallet Page

---
+++ views/DashboardWalletPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Button, TouchableOpacity, Pressable, StyleSheet, } from "react-native";
+
+const DashboardWalletPage = () => {
+    return (
+        <View>
+            <Text>Wallet</Text>
+        </View>
+    );
+};
+
+export default DashboardWalletPage;
