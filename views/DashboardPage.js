8273993f91379ec0698ec8c1543d7e34f80f7f31
Gregg Wong <gwong@smartterm.io>
2023-04-04 17:10:43 -0500

    Link screens for tabs

--- views/DashboardPage.js
+++ views/DashboardPage.js
@@ -1,23 +1,39 @@
 import React from "react";
-import { View, Text, Button, TouchableOpacity, Pressable, StyleSheet } from "react-native";
+import { View, Text, Button, TouchableOpacity, Pressable, StyleSheet, } from "react-native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import DashboardHomePage from "./DashboardHomePage";
+import DashboardWalletPage from "./DashboardWalletPage";
+
+const Drawer = createDrawerNavigator();
 
 const Tab = createBottomTabNavigator();
 const DashboardPage = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Wallet" component={Home} />
+            <Tab.Screen name="Home" component={DashboardHomePage} options={{ headerShown: false }} />
+            <Tab.Screen name="Wallet" component={DashboardWalletPage} />
         </Tab.Navigator>
     );
 }
 
-const Home = () => {
+
+const HomeV2 = () => {
     return (
         <View>
             <Text>Home</Text>
         </View>
     );
 };
+
+const drawerNavigatorScreenOptions = {
+}
+
+const drawerScreenOptions = {
+    screenOne: {
+        title: 'Screen One',
+        // head
+    }
+}
 
 export default DashboardPage;
