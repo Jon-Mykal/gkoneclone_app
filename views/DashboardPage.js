f23d82e19581097456f76c427b49f261c787aa1e
Gregg Wong <gwong@smartterm.io>
2023-04-04 23:18:22 -0500

    Implement Material Bottom Tabs

--- views/DashboardPage.js
+++ views/DashboardPage.js
@@ -1,18 +1,39 @@
 import React from "react";
 import { View, Text, Button, TouchableOpacity, Pressable, StyleSheet, } from "react-native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import DashboardHomePage from "./DashboardHomePage";
 import DashboardWalletPage from "./DashboardWalletPage";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 const DashboardPage = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={DashboardHomePage} options={{ headerShown: false }} />
-            <Tab.Screen name="Wallet" component={DashboardWalletPage} />
+        <Tab.Navigator activeColor="red"
+            inactiveColor="grey"
+            barStyle={{ backgroundColor: 'white' }}>
+            <Tab.Screen name="Home"
+                component={DashboardHomePage}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={26} />
+                    ),
+                }} />
+            <Tab.Screen name="Wallet" 
+                component={DashboardWalletPage}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="wallet" color={color} size={26} />
+                    ),
+                }}  />
         </Tab.Navigator>
     );
 }
@@ -20,9 +41,9 @@ const DashboardPage = () => {
 
 const HomeV2 = () => {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={DashboardHomePage} />
+        </Stack.Navigator>
     );
 };
 
