33619224b9a85d74b0414af6f200073db1e3caf2
Gregg Wong <gwong@smartterm.io>
2023-04-04 23:17:51 -0500

    Add Drawer Page Links

--- views/DashboardHomePage.js
+++ views/DashboardHomePage.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
-import { View, Text, Button, TouchableOpacity, Pressable, StyleSheet, } from "react-native";
-import { createDrawerNavigator, useDrawerStatus } from '@react-navigation/drawer';
+import { View, Text, Button, TouchableOpacity, Pressable, StyleSheet, ScrollView } from "react-native";
+import { DrawerContentScrollView, DrawerItem, createDrawerNavigator, useDrawerStatus } from '@react-navigation/drawer';
 import { DrawerActions, useNavigation } from '@react-navigation/native';
+import { Box, HStack, VStack } from '@react-native-material/core';
 
 const Drawer = createDrawerNavigator();
+
+const CustomDrawerContent = (props) => {
+    return (
+        <DrawerContentScrollView {...props}>
+            <DrawerItem label="Rewards" onPress={() => alert('Rewards')} />
+            <DrawerItem label="Tell us your opinion" onPress={() => alert('Tell us your opinion')} />
+            <DrawerItem label="Help & Contact" onPress={() => alert('Help & Contact')} />
+        </DrawerContentScrollView>
+    );
+};
 
 const DashboardHomePage = ({ navigation }) => {
     const nav = navigation;
@@ -12,17 +23,16 @@ const DashboardHomePage = ({ navigation }) => {
 
     return (
         <Drawer.Navigator
-            
+            drawerContent={props => <CustomDrawerContent {...props} />}
             defaultStatus="closed"
             screenOptions={{
                 drawerPosition: 'right',
                 drawerActiveTintColor: 'red',
                 headerTintColor: 'red',
                 drawerType: 'front',
-                // drawerHideStatusBarOnOpen: true,
                 headerRight: () => (
                     <View>
-                        <Pressable style={{ padding: 15}} onPress={() => {setToggle(!toggle); alert(toggle)}}>
+                        <Pressable style={{ padding: 15 }} onPress={() => { setToggle(!toggle); alert(toggle) }}>
                             <Text>Header Right</Text>
                         </Pressable>
                     </View>
@@ -49,7 +59,7 @@ const DashboardHomePage = ({ navigation }) => {
                     // drawerPosition: 'left',
 
                 }} />
-            <Drawer.Screen name="TellOpinion"
+            {/* <Drawer.Screen name="TellOpinion"
                 component={HomeV2}
                 options={{
                     title: 'Tell us your opinion',
@@ -74,16 +84,68 @@ const DashboardHomePage = ({ navigation }) => {
                     // headerShown: false,
                     // drawerPosition: 'left',
 
-                }} />
+                }} /> */}
         </Drawer.Navigator>
     );
 };
 
 const HomeV2 = ({ route }) => {
     return (
-        <View>
-            <Text>{route.name}</Text>
-        </View>
+        <ScrollView>
+            <VStack spacing={10}>
+                <View style={{ height: 320, width: '100%', backgroundColor: 'gray' }}></View>
+                <View style={{ height: 320, width: '100%', backgroundColor: 'lightgray', alignItems: 'center', justifyContent: 'center' }}>
+                    <VStack spacing={15}>
+                        <HStack spacing={15}>
+                            {
+                                ["Receive Money", "Pay Bills", "Credit Card"].map((item, index) => {
+                                    return (
+                                        <Pressable style={{ alignItems: 'center' }} key={index} onPress={() => alert(item)}>
+                                            <Box h={100} w={110} style={{ backgroundColor: 'white', borderRadius: 10, alignItems: 'center', justifyContent: 'center' }}>
+                                                <Text>{item}</Text>
+                                            </Box>
+                                        </Pressable>
+                                    )
+                                })
+                            }
+                        </HStack>
+                        <HStack spacing={15}>
+                            {
+                                ["Rewards", "Champs Spirit"].map((item, index) => {
+                                    return (
+                                        <Pressable style={{ alignItems: 'center' }} key={index} onPress={() => alert(item)}>
+                                            <Box h={100} w={110} style={{ backgroundColor: 'white', borderRadius: 10, alignItems: 'center', justifyContent: 'center' }}>
+                                                <Text>{item}</Text>
+                                            </Box>
+                                        </Pressable>
+                                    )
+                                })
+                            }
+                        </HStack>
+                    </VStack>
+                </View>
+                <View style={{ height: 200, width: '100%', backgroundColor: 'white', alignItems: 'center', flexDirection: 'row' }}>
+                    <ScrollView horizontal={true}>
+                        <HStack spacing={12} mh={20}>
+                            {
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => {
+                                    return (
+                                        <Pressable style={{ alignItems: 'center', justifyContent: 'center' }} key={index} onPress={() => alert(item)}>
+                                            <Box h={90} w={90} style={{ backgroundColor: 'lightblue', borderRadius: 50, alignItems: 'center', justifyContent: 'center' }}>
+                                                <Text>Box {item}</Text>
+                                            </Box>
+                                            <Text style={{ marginTop: 10}}>Bill {item}</Text>
+                                        </Pressable>
+                                    )
+                                })
+                            }
+                        </HStack>
+                    </ScrollView>
+                </View>
+                <View style={{ height: 320, width: '100%', backgroundColor: 'red' }}></View>
+            </VStack>
+
+        </ScrollView>
     );
 };
 
