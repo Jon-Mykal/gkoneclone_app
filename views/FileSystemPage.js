f6b68ce0c6784e2cf6fa5323d089ce0f631881d3
Gregg Wong <gwong@smartterm.io>
2023-04-16 19:54:09 -0500

    Add FileSystemPage UI

---
+++ views/FileSystemPage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FileSystemPage = () => {
+  return (
+    <View>
+      <Text>FileSystemPage</Text>
+    </View>
+  )
+}
+
+export default FileSystemPage
+
+const styles = StyleSheet.create({})
